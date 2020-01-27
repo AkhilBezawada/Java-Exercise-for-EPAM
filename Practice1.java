bbc05cb577d041517262cb1d38c2518aa900113a
Naga Sai Akhil Bezawada <50613096+AkhilBezawada@users.noreply.github.com>
2020-01-27 14:01:56 +0530

    Update Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -1,7 +1,7 @@
-class A
+class Practice1
 {
  public static void main(String args[])
  {
-  System.out.println(args[0] + "technologies" + args[1]);
+  System.out.print(args[0] + " technologies " + args[1]);
    }
 }
